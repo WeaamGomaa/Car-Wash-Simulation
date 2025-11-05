40c704a2e20c3dcf9136fbf920e3043db6b9d4ef
WeaamGomaa <weeamgomaa@gmail.com>
2025-11-05 15:12:00 +0200

    Initializing shared resources, creating and starting threads

--- ServiceStation.java
+++ ServiceStation.java
@@ -1,7 +1,9 @@
-import java.io.*;
 import java.util.*;
 
 class Semaphore{
+    Semaphore(int sem){
+
+    }
 
 }
 
@@ -32,12 +34,32 @@ class Pump extends Thread{
 }
 
 public class ServiceStation {
-    private Queue<Car> waitingQueue;
-    private Semaphore empty;
-    private Semaphore full;
-    private Semaphore mutex;
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("=====Welcome in our Car Wash Simulation=====");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Waiting area capacity: ");
+        int waitingAreaCapacity = input.nextInt();
+        System.out.println("Number of service bays (pumps): ");
+        int numberOfPumps = input.nextInt();
+        System.out.println("Number of cars arriving: ");
+        int totalCars = input.nextInt();
 
+        //Initializing shared resources
+        Queue<Car> waitingQueue = new LinkedList<>();
+        Semaphore empty = new Semaphore(waitingAreaCapacity);
+        Semaphore full = new Semaphore(0);
+        Semaphore mutex = new Semaphore(1);
+
+        //Create and start Pump threads
+        for(int i = 0; i < numberOfPumps; i++){
+            new Pump(waitingQueue, empty, full, mutex, i+1).start();
+        }
+
+        //Create and start Car threads
+        for(int i = 0; i < totalCars; i++){
+            new Car(waitingQueue, empty, full, mutex, "C"+i+1).start();
+            Thread.sleep(2000);
+        }
     }
 }
